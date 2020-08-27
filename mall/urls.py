7e15a93560f6ed4f8dac9e6f5063d1348fb67d1c
hhdMrLion <55070348+hhdMrLion@users.noreply.github.com>
2020-08-27 16:10:32 +0800

    '实现商品分页并刷新和优化商品搜索'

--- mall/urls.py
+++ mall/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import url
 from mall import views
 
 urlpatterns = [
-    # 商品列表
-    url(r'^prod/list/$', views.product_list, name='product_list'),
+    # 商品列表 def function 来实现
+    # url(r'^prod/list/$', views.product_list, name='product_list'),
+    # 使用class 用面向对像的方法来实现
+    url(r'^prod/list/$', views.ProductList.as_view(), name='product_list'),
+    # 加载HTML片段的地址
+    url(r'^prod/load/list/$', views.ProductList.as_view(
+        template_name='product_load_list.html'
+    ), name='product_load_list'),
     # 商品详情
     url(r'^prod/detail/(?P<pk>\S+)/$', views.product_detail, name='product_detail'),
 ]
--- mall/views.py
+++ mall/views.py
@@ -1,6 +1,9 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
 from mall.models import Product
 from utils import constants
 
@@ -20,3 +23,25 @@ def product_list(request):
 def product_detail(request, template_name='product_detail.html'):
     """商品详情"""
     return render(request, template_name)
+
+
+class ProductList(ListView):
+    """商品列表"""
+    # 每页放多少条数据
+    paginate_by = 5
+    # 模板位置
+    template_name = 'product_list.html'
+
+    def get_queryset(self):
+        query = Q(status=constants.PRODUCT_STATUS_SELL, is_valid=True)
+        # 按名称搜索
+        name = self.request.GET.get('name', '')
+        if name:
+            query = query & Q(name__icontains=name)
+        return Product.objects.filter(query)
+
+    def get_context_data(self, **kwargs):
+        """添加额外的参数，添加搜索参数"""
+        context = super().get_context_data(**kwargs)
+        context['search_data'] = self.request.GET.dict()
+        return context
